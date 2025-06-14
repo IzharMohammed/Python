a7436addc0bf9736e4a010143f594bc3c0ff415b
IzharMohammed <izharmohammed21@gmail.com>
2025-06-14 12:01:16 +0530

    Added operators 1

---
+++ 1-python basics/1.3-operators.py
@@ -0,0 +1,80 @@
+'''
+1. Arithmetic Operators,
+ - Addition,
+  - Subtraction,
+   - Multiplication,
+   - Division,
+   - Floor Division,
+   - Modulus,
+   - Exponentiation,
+ 2. Comparison Operators,
+  - Equal to,
+   - Not equal to,
+   - Greater than,
+   - Less than,
+   - Greater than or equal to ,
+   - Less than or equal to,
+ 3. Logical Operators,
+   - AND,
+   - OR,
+   - NOT,
+'''
+
+## Arithmethic Operation,
+a=10
+b = 5
+    
+add_result=a+b  #addiiton,
+sub_result=a-b  #substraction ,
+mult_result=a*b #multiplication,
+div_result=a/b  #division,
+floor_div_result=a//b ## floor division,
+modulus_result=a%b #modulus operation,
+exponent_result=a**b ## Exponentiation,
+    
+print(add_result),
+print(sub_result),
+print(mult_result),
+print(div_result),
+print(floor_div_result),
+print(modulus_result),
+print(exponent_result),
+
+print("--------------------------")
+## Comparison Operators
+
+## == Equal to
+a=10
+b=10
+print(a==b)
+
+str1="izhar"
+str2="izhar"
+print(str1==str2)
+
+## Not equal to !=
+print(str1!=str2)
+
+str3="izhar"
+str4="Izhar"
+print(str3!=str4)
+
+## Greater than >
+num1=10
+num2=20
+
+print(num1> num2)
+
+## less than <
+print(num1 < num2)
+
+print("---------------")
+## Greater than or equal to
+number1=45
+number2=45
+print(number1 >= number2)
+
+## less than or equal to
+number1=44
+number2=45
+print(number1 <= number2)
