d00a4bb0ed82a845593b2032c81970c9b4bf1073
IzharMohammed <izharmohammed21@gmail.com>
2025-06-14 12:00:36 +0530

    Added data types

---
+++ 1-python basics/1.2-Datatypes.py
@@ -0,0 +1,28 @@
+## DataTypes
+#### 1. Definition:
+
+    ## - Data types are a classification of data which tell the compiler or interpreter how the programmer intends to use the data
+    ## They determine the type of operations that can be performed on the data, the values that the data can take, and the amount of memory needed to store the data,
+    
+#### 2. Importance of Data Types in Programming
+## Explanation:
+    #- Data types ensure that data is stored in an efficient way,
+    #- They help in performing correct operations on data.,
+    #- Proper use of data types can prevent errors and bugs in the program."
+
+    #1. Introduction to Data Types\n",
+    #2. Importance of Data Types in Programming\n",
+    #3. Basic Data Types\n",
+    #   - Integers\n",
+    #   - Floating-point numbers\n",
+    #   - Strings\n",
+    #   - Booleans\n",
+    # 4. Advanced Data Types\n",
+    #   - Lists\n",
+    #  - Tuples\n",
+    #   - Sets\n",
+    #   - Dictionaries\n",
+    #5. Type Conversion\n",
+
+result="izhar"+5
+print(result) ## TypeError: can only concatenate str (not "int") to str
