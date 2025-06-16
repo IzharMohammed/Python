77c9b28abf3a1fd9c2cb94442158bcf537c2aee3
IzharMohammed <izharmohammed21@gmail.com>
2025-06-16 10:44:10 +0530

    Added list methods

--- 3-Data Structures/3.1-Lists.py
+++ 3-Data Structures/3.1-Lists.py
@@ -21,3 +21,42 @@ print(fruits[-1])
 
 print(fruits[2:])
 print(fruits[1:3])
+
+print("-----------------")
+
+### Modifying list elements
+fruits[4]="mango"
+print(fruits)
+
+### List methos
+fruits.append("orange") ## Add an item to the end 
+print(fruits)
+
+fruits.insert(2,"watermelon")
+print(fruits)
+
+fruits.remove("kiwi") ## Removing the 1st occurence
+print(fruits)
+
+print("-----------------------")
+
+## Remove and return the last
+popped_fruits=fruits.pop()
+print(popped_fruits)
+print(fruits)
+
+print(fruits.index("mango"))
+
+print("------------")
+fruits.insert(2,"banana")
+print(fruits)
+print(fruits.count("banana"))
+
+fruits.sort() ## sort the list in ascending order
+print(fruits)
+
+fruits.reverse() ## Reverse the list
+print(fruits)
+
+fruits.clear() ## Remove all items from th list
+print(fruits)
