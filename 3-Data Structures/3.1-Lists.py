20226fa2b172286a190819a217aa5047a95e7f36
IzharMohammed <izharmohammed21@gmail.com>
2025-06-16 10:25:44 +0530

    Added intro to lists

---
+++ 3-Data Structures/3.1-Lists.py
@@ -0,0 +1,16 @@
+'''
+Lists:- 
+
+Lists are ordered, mutable collections of items.
+They can contain items of different data types.
+'''
+list=[]
+print(type(list))
+
+
+names=["izhar","mohammed","unknmown",1,2,3]
+print(names)
+
+mixed_lists=[1,"Hello",3.14,True,False]
+print(mixed_lists)
+
