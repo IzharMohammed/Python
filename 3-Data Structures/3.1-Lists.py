49f5195f296172fdf0d4c787fbfbc56d2c5a843a
IzharMohammed <izharmohammed21@gmail.com>
2025-06-16 11:07:45 +0530

    Added list comprehension

--- 3-Data Structures/3.1-Lists.py
+++ 3-Data Structures/3.1-Lists.py
@@ -91,6 +91,54 @@ for number in numbers:
 for index,number in enumerate(numbers):
     print(index,number)
 
+## List comprehension
+lst=[]
+for x in range(10):
+    lst.append(x**2)
+print(lst)
+# [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
+print([x**2 for x in range(10)])
+# [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
+print("-----------")
+## List Comprehension
+## Basics Syantax             [expression for item in iterable]
+## with conditional logic     [expression for item in iterable if condition]
+## Nested List Comprehension  [expression for item1 in iterable1 for item2 in iterable2]
 
+##Basic list comprehension
+
+square=[num**2 for num in range(10)]
+print(square)
+
+## List compre. with condition
+lst=[]
+for i in range(10):
+    if i%2==0:
+        lst.append(i)
+print(lst)
+
+even_numbers=[num for num in range(10) if num%2==0]
+print(even_numbers)
+
+## Nested list comprehension
+lst1=[1,2,3,4]
+lst2=['a','b','c','d']
+
+pair=[[i,j] for i in lst1 for j in lst2]
+print(pair)
+## [[1, 'a'], [1, 'b'], [1, 'c'], [1, 'd'], [2, 'a'], [2, 'b'], [2, 'c'], [2, 'd'], [3, 'a'], [3, 'b'], [3, 'c'], [3, 'd'], [4, 'a'], [4, 'b'], [4, 'c'], [4, 'd']]
+
+## List comprehension with function calls
+words = ["hello", "world", "python", "list", "comprehension"]
+length=[len(word) for word in words]
+print(length)
+## [5, 5, 6, 4, 13]
+
+'''
+Conclusion
+List comprehensions are a powerful and concise way to create lists in Python. 
+They are syntactically compact and can replace more verbose looping constructs. 
+Understanding the syntax of list comprehensions will help you write cleaner and more efficient Python code.
+'''
