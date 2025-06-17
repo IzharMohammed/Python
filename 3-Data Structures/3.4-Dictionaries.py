849b6b2c0354f32d86b2409fc2357f53a993e1c1
IzharMohammed <izharmohammed21@gmail.com>
2025-06-17 11:48:22 +0530

    Accessing of dictionaries

--- 3-Data Structures/3.4-Dictionaries.py
+++ 3-Data Structures/3.4-Dictionaries.py
@@ -23,17 +23,39 @@ print(type(empty_dict))  # <class 'dict'>
 empty_dict=dict()
 print(empty_dict)        # {}
 
-student={"name":"Krish","age":32,"grade":24}
-print(student)        # {'name': 'Krish', 'age': 32, 'grade': 24}  
+student={"name":"izhar","age":32,"grade":24}
+print(student)        # {'name': 'izhar', 'age': 32, 'grade': 24}  
 print(type(student))  # <class 'dict'>
 
 # Single key is slways used
-student={"name":"Krish","age":32,"name":24}
+student={"name":"izhar","age":32,"name":24}
 print(student)  # {'name': 24, 'age': 32}
 
+## accessing Dictionary Elements
+student={"name":"izhar","age":32,"grade":'A'}
+print(student)      # {'name': 'izhar', 'age': 32, 'grade': 'A'}
 
+print("----------------------")
+## Accessing Dictionary elements
+print(student['grade']) # A
+print(student['age'])   # 32
 
+## Accessing using get() method
+print(student.get('grade'))       # A
+print(student.get('last_name'))   # None  
+print(student.get('last_name',"Not Available")) # Not Available
 
+## Modifying Dicitonary Elements
+## Dictionary are mutable,so you can add, update or delete elements
+print(student) # {'name': 'izhar', 'age': 32, 'grade': 'A'}
+
+student["age"]=99             ##update value for the key
+print(student)                # {'name': 'izhar', 'age': 99, 'grade': 'A'}
+student["address"]="India"    ## added a new key and value
+print(student)                # {'name': 'izhar', 'age': 99, 'grade': 'A', 'address': 'India'}
+
+del student["grade"]
+print(student)                 # {'name': 'izhar', 'age': 99, 'address': 'India'}
 
 
 
