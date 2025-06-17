52513e0f79ee796471a1028dbecdc3efc94bf728
IzharMohammed <izharmohammed21@gmail.com>
2025-06-17 11:24:32 +0530

    set mathematical operations

--- 3-Data Structures/3.3-Sets.py
+++ 3-Data Structures/3.3-Sets.py
@@ -68,4 +68,32 @@ my_set={1,2,3,4,5}
 print(3 in my_set)      # True
 print(10 in my_set)     # False
 
+print("-----------------")
+## Mathematical Operation
+set1={1,2,3,4,5,6}
+set2={4,5,6,7,8,9}
+
+## Union
+union_set=set1.union(set2)
+print(union_set)            # {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+## Intersection
+intersection_set=set1.intersection(set2)
+print(intersection_set)     # {4, 5, 6}
+
+set1.intersection_update(set2)
+print(set1)                 # {4, 5, 6}
+# print(set2)
+
+
+print("----------------")
+set1={1,2,3,4,5,6}
+set2={4,5,6,7,8,9}
+
+## Difference 
+print(set1.difference(set2)) # {1,2,3}
+print(set2.difference(set1)) # {8,9,7}  
+
+## Symmetric Difference
+print(set1.symmetric_difference(set2)) # {1, 2, 3, 7, 8, 9}
 
