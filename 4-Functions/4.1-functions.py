4021a67843ad8207f7249f2077df957029a34fdd
IzharMohammed <izharmohammed21@gmail.com>
2025-06-17 12:37:26 +0530

    Added info about functions

--- 4-Functions/4.1-functions.py
+++ 4-Functions/4.1-functions.py
@@ -12,61 +12,65 @@ Return Statement
 
 ## Introduction to Functions
 ## Definition:
-
 ## A function is a block of code that performs a specific task. Functions help in organizing code, reusing code, and improving readability.
 
-
-num=24
-if num%2==0:
+# Example without function
+num = 24
+if num % 2 == 0:
     print("even")
 else:
     print("odd")
 
+# Defining a simple function
 def even_or_odd(num):
     """This function finds even or odd"""
-    if num%2==0:
+    if num % 2 == 0:
         print("the number is even")
     else:
         print("the number is odd")
 
-
-## Call this function
+## Calling the function
 even_or_odd(24)
 
+## Function with multiple parameters and return value
+def add(a, b):
+    """Adds two numbers and returns the result"""
+    return a + b
 
-## function with multiple parameters
-
-def add(a,b):
-    return a+b
-
-result=add(2,4)
+result = add(2, 4)
 print(result)
 
 ## Default Parameters
-
+# Function with required parameter
 def greet(name):
+    """Greets a person by name"""
     print(f"Hello {name} Welcome To the paradise")
 
 greet("izhar")
 
+# Function with default parameter value
 def greet(name="Guest"):
+    """Greets a person by name, defaults to 'Guest'"""
     print(f"Hello {name} Welcome To the paradise")
 
-greet()
-greet("izhar")
+greet()  # Uses default value
+greet("izhar")  # Overrides default value
 '''
 Hello Guest Welcome To the paradise
 Hello izhar Welcome To the paradise
 '''
 
 ### Variable Length Arguments
-## Positional And Keywords arguments
+## *args - Accepts any number of positional arguments
 
+# Using arbitrary argument name (izhar in this case)
 def print_numbers(*izhar):
+    """Prints all provided arguments"""
     for number in izhar:
         print(number)
 
-print_numbers(1,2,3,4,5,6,7,8,"izhar")
+print_numbers(1, 2, 3, 4, 5, 6, 7, 8, "izhar")
+
 '''
 1
 2
@@ -79,13 +83,13 @@ print_numbers(1,2,3,4,5,6,7,8,"izhar")
 izhar
 '''
 
-## Positional arguments
+# Convention is to use *args for variable positional arguments
 def print_numbers(*args):
+    """Prints all provided positional arguments"""
     for number in args:
         print(number)
 
-
-print_numbers(1,2,3,4,5,6,7,8,"izhar")     
+print_numbers(1, 2, 3, 4, 5, 6, 7, 8, "izhar")     
 '''
 1
 2
@@ -99,16 +103,20 @@ izhar
 '''
 
 print("-----------------")
-### Keywords Arguments
+### **kwargs - Accepts any number of keyword arguments
 
-def print_details(*args,**kwargs):
+def print_details(*args, **kwargs):
+    """
+    Prints both positional and keyword arguments
+    *args collects positional arguments as tuple
+    **kwargs collects keyword arguments as dictionary
+    """
     for val in args:
         print(f"positional argument: {val}")
-    for key,val in kwargs.items():
+    for key, val in kwargs.items():
         print(f"{key} : {val}")
 
-print_details(1,2,3,4,"izhar",name="izhar",age="21",country="India")
-
+print_details(1, 2, 3, 4, "izhar", name="izhar", age="21", country="India")
 '''
 positional argument: 1
 positional argument: 2
@@ -121,13 +129,16 @@ country : India
 '''
 
 ### Return statements
-def multiply(a,b):
-    return a*b
+# Function returning a single value
+def multiply(a, b):
+    """Multiplies two numbers and returns the result"""
+    return a * b
 
-print(multiply(2,3))  # 6
+print(multiply(2, 3))  # 6
 
-### Return multiple parameters
-def multiply(a,b):
-    return a*b , a , b
+### Returning multiple values (as a tuple)
+def multiply(a, b):
+    """Returns the product and both input numbers"""
+    return a * b, a, b
 
-print(multiply(2,3)) # (6, 2, 3)
+print(multiply(2, 3))  # (6, 2, 3)
